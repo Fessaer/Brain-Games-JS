5837803ad4ca20f6bd5782d7261ff934cbbe3acd
Fessaer <nwtimon@yandex.ru>
2020-03-07 18:50:46 +0500

    '07032020'

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
 
 import readlineSync from 'readline-sync';
-let actual = readlineSync.question('May I have your name? ');
-console.log("Hello, " + actual);
+
+console.log('Welcome to the Brain Games!');
+
+const actual = readlineSync.question('My I have your name? ');
+
+console.log(`Hello, ${actual}`);
--- src/index.js
+++
@@ -1,3 +0,0 @@
-import readlineSync from 'readline-sync';  
-let name = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + name);
